5d6500f3df004f1f542c8ccd8a75c94db89ed206
amitnos7 <amitnos7@gmail.com>
2024-12-21 18:27:07 +0200

    FileTask.js

--- FileTask.js
+++ FileTask.js
@@ -1,26 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-// Define the directory path containing the input files
+// Directory path
 const dirPath = path.join(__dirname, "/OutputFolder");
-
-// Initialize an empty string to hold the merged content
 let mergedContent = "";
 
-// Iterate through 10 files
+
 for (let i = 1; i <= 10; i++) {
   
-    // Read the content of the current file
     const filePath = path.join(dirPath, `${i}.txt`);
     const fileContent = fs.readFileSync(filePath, "utf-8");
 
-    // Split the content into lines
     const lines = fileContent.split("\r\n");
-
-    // Extract the required number of lines (i lines from file i)
     const selectedLines = lines.slice(0, i);
 
-    // Add the selected lines to the merged content
+   
     mergedContent += selectedLines.join("\r\n") + "\n";
 
     console.log(
